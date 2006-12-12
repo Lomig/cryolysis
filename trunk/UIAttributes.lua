97f233b9de5780d2db16fb46451546d6b7d1ac16
Eternally777 <Eternally777@c0bc482e-5624-0410-b34c-f5020ee5ef57>
2006-12-12 04:38:28 +0000

    * Food button works (again, except for the trade functionality)

--- trunk/UIAttributes.lua
+++ trunk/UIAttributes.lua
@@ -13,5 +13,19 @@ function Cryolysis_UpdateDrinkButtonAttributes()
 		f:SetAttribute("*type2", "spell")
 		f:SetAttribute("spell2", "Conjure Water")
 	end
-	DEFAULT_CHAT_FRAME:AddMessage("Attributes set.")
+end
+
+function Cryolysis_UpdateFoodButtonAttributes()
+	local f = _G["CryolysisFoodButton"]
+	assert(f, "Error: Food button not created or named properly!")
+	if ( not InCombatLockdown() ) then
+		f:SetAttribute("*type1", "item")
+		local highestFoodName = GetItemInfo(CryolysisPrivate.highestFoodId)
+		if ( highestFoodName ) then
+			f:SetAttribute("item1", highestFoodName)
+		end
+		
+		f:SetAttribute("*type2", "spell")
+		f:SetAttribute("spell2", "Conjure Food")
+	end
 end
