3fec2c5b62f588620537e097fbd063477018beb4
Dunwael <Dunwael@c0bc482e-5624-0410-b34c-f5020ee5ef57>
2006-12-12 02:59:52 +0000

    * File Header changed

--- trunk/Localization-dialog-fr.lua
+++ trunk/Localization-dialog-fr.lua
@@ -1,14 +1,36 @@
+--[[
+    Cryolysis
+    Copyright (C) 2006 Cryolysis: Reborn Team
+
+    This file is part of Cryolysis.
+
+    Cryolysis is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Cryolysis is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cryolysis; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--]]
+
+
 ------------------------------------------------------------------------------------------------------
 -- Cryolysis
 --
--- Based on Necrosis LdC by Lomig and Nyx (http://necrosis.larmes-cenarius.net)
--- Original Necrosis Idea : Infernal (http://www.revolvus.com/games/interface/necrosis/)
--- Cryolysis Maintainer : Kaeldra of Aegwynn
+-- Copyright (c) 2006 Cryolysis: Reborn Team
+-- Copyright (c) 2006 Kaeldra (darklyte@gmail.com)
+-- Copyright (c) 2005-2006 Lom Enfroy
 --
--- Contact : darklyte@gmail.com
--- Send me in-game mail!  Yersinia on Aegwynn, Horde side.
--- Guild: <Working as Intended>
--- Version Date: 07.05.2006
+-- Skins: Eliah, Ner'zhul FR
+--
+--
+-- Version 12.12.2006
 ------------------------------------------------------------------------------------------------------
 
 
