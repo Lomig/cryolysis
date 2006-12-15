ee9605ba9bea664d561a547e5d790316994188e1
Dunwael <Dunwael@c0bc482e-5624-0410-b34c-f5020ee5ef57>
2006-12-15 06:01:53 +0000

    GPL compliant :)

--- UIAttributes.lua
+++ UIAttributes.lua
@@ -1,3 +1,43 @@
+--[[
+    Cryolysis
+    Copyright (C) 2006 Cryolysis: Reborn Team
+
+    This file is part of Cryolysis.
+
+    Cryolysis is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Cryolysis is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cryolysis; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Cryolysis: Reborn Team (Alphabetically sorted)
+        - Eternally777
+	- Lom Enfroy
+	- Thomas Hart II
+--]]
+
+
+------------------------------------------------------------------------------------------------------
+-- Cryolysis
+--
+-- Copyright (c) 2006 Cryolysis: Reborn Team
+-- Copyright (c) 2006 Kaeldra (darklyte@gmail.com)
+-- Copyright (c) 2005-2006 Lom Enfroy
+--
+-- Skins: Eliah, Ner'zhul FR
+--
+--
+-- Version 12.12.2006
+------------------------------------------------------------------------------------------------------
+
 local _G = getfenv(0)
 
 function Cryolysis_UpdateDrinkButtonAttributes()
