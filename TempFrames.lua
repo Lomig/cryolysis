18c2d899d2adf4c9df720588ea210231b0fdc19e
Eternally777 <Eternally777@c0bc482e-5624-0410-b34c-f5020ee5ef57>
2006-12-22 03:27:55 +0000

    * Main sphere buttons are now Lua generated.

--- TempFrames.lua
+++ TempFrames.lua
@@ -83,21 +83,40 @@ function Cryo:CreateMainButton()
 	self.frames.main = f
 end
 
-local function newButton(name, normalTexture, highlightTexture)
-	local f = CreateFrame("Button", name, UIParent, "SecureActionButtonTemplate")
+local function newButton(name, normalTexture, highlightTexture, fontString, isMenu)
+	local template
+	if ( not isMenu ) then
+		template = "SecureActionButtonTemplate"
+	else
+		template = "SecureAnchorUpDownTemplate"
+	end
+	local f = CreateFrame("Button", name, UIParent, template)
 	f:RegisterForDrag("LeftButton")
 	f:RegisterForClicks("AnyUp")
 	f:SetMovable(true)
 	
 	f:SetWidth(34)
 	f:SetHeight(34)
-	f:SetPoint("CENTER", UIParent, "CENTER", -17, -100)
 	
 	if ( normalTexture ) then
 		f:SetNormalTexture( string.format(Cryo.textureDir, normalTexture) )
 	end
 	if ( highlightTexture ) then
 		f:SetHighlightTexture( string.format(Cryo.textureDir, highlightTexture) )
+	end
+	
+	if ( not fontString ) then
+		fontString = "$parentText"
+	end
+	local t = f:CreateFontString(fontString, "ARTWORK")
+	t:SetFontObject(_G["GameFontNormal"])
+	t:SetTextColor(1.0, 1.0, 1.0, 1.0)
+	t:SetPoint("CENTER", f, "CENTER", 0, 0)
+	f.text = t
+	
+	if ( isMenu ) then
+		f.header = CreateFrame("Frame", isMenu, f, "SecureStateHeaderTemplate")
+		f.header:Hide()
 	end
 	
 	f:SetScript("OnLeave", function()
@@ -125,16 +144,86 @@ function Cryo:CreateButtons()
 	f:SetScript("OnEnter", function()
 		self:BuildSpellTooltip(this, "Left", LEFT)
 	end)
+	
+	f = newButton("CryolysisEvocationButton", "EvocationButton-01", "BaseMenu-02", "CryolysisEvocationCooldown")
+	f:SetScript("OnEnter", function()
+		self:BuildSpellTooltip(this, "Evocation", LEFT)
+	end)
+	
+	f = newButton("CryolysisRightSpellButton", "Shard", "BaseMenu-02")
+	f:SetScript("OnEnter", function()
+		self:BuildTooltip(this, "Right", RIGHT)
+	end)
+	
+	f = newButton("CryolysisDrinkButton", "DrinkButton-01", "BaseMenu-02", "CryolysisDrinkCount")
+	f:SetScript("OnEnter", function()
+		self:BuildTooltip(this, "Drink", LEFT)
+	end)
+	
+	f = newButton("CryolysisFoodButton", "FoodButton-01", "BaseMenu-02", "CryolysisFoodCount")
+	f:SetScript("OnEnter", function()
+		self:BuildTooltip(this, "Food", LEFT)
+	end)
+	
+	f = newButton("CryolysisMountButton", "MountButton-01", "BaseMenu-02")
+	f:SetScript("OnEnter", function()
+		self:BuildTooltip(this, "Mount", RIGHT)
+	end)
+	
+	f = newButton("CryolysisManaStoneButton", "ManastoneButton-01", "BaseMenu-02", "CryolysisManastoneCooldown", "CryolysisManaStoneMenu0")
+	f:SetScript("OnEnter", function()
+		self:BuildTooltip(this, "Manastone", LEFT)
+	end)
+	
+	f = newButton("CryolysisBuffMenuButton", "BuffMenuButton-01", "BaseMenu-02", nil, "CryolysisBuffMenu0")
+	f:SetScript("OnEnter", function()
+		self:BuildTooltip(this, "Buff", LEFT)
+	end)
+	
+	f = newButton("CryolysisPortalMenuButton", "PortalMenuButton-01", "BaseMenu-02", nil, "CryolysisPortalMenu0")
+	f:SetScript("OnEnter", function()
+		self:BuildTooltip(this, "Portal", RIGHT)
+	end)
 end
 
 --[[
-	<Button name="CryolysisLeftSpellButton" inherits="CryolysisSubButtonTemplate">
+	<Button name="CryolysisPortalMenuButton" inherits="SecureAnchorUpDownTemplate" hidden="false" enableMouse="true"  toplevel="true" parent="UIParent" movable="true">
+		<Size>
+			<AbsDimension x="34" y="34"/>
+		</Size>
+		<Anchors>
+			<Anchor point="CENTER" relativeTo="UIParent" relativePoint="CENTER">
+				<Offset>
+					<AbsDimension x="17" y="-100"/>
+				</Offset>
+			</Anchor>
+		</Anchors>
 		<Scripts>
+			<OnLoad>
+				this:RegisterForDrag("LeftButton");
+				this:RegisterForClicks("LeftButtonUp", "MiddleButtonUp", "RightButtonUp");
+				HideUIPanel(this);
+			</OnLoad>
 			<OnEnter>
-				Cryo:BuildSpellTooltip(this, "Left", "ANCHOR_LEFT");
+				Cryo:BuildTooltip(this, "Portal", "ANCHOR_RIGHT");
 			</OnEnter>
+			<OnLeave>
+				GameTooltip:Hide();
+			</OnLeave>
+			<OnDragStart>
+				if not CryolysisLockServ then Cryo:OnDragStart(this); end
+			</OnDragStart>
+			<OnDragStop>
+				Cryo:OnDragStop(this);
+			</OnDragStop>
+			<OnMouseUp>
+				Cryo:OnDragStop(this);
+			</OnMouseUp>
 		</Scripts>
-		<NormalTexture file="Interface\AddOns\Cryolysis\UI\Shard"/>
+		<Frames>
+			<Frame name="CryolysisPortalMenu0" inherits="SecureStateHeaderTemplate"/>
+		</Frames>
+		<NormalTexture file="Interface\AddOns\Cryolysis\UI\PortalMenuButton-01"/>
 		<HighlightTexture file="Interface\AddOns\Cryolysis\UI\BaseMenu-02"/>
 	</Button>
 --]]
